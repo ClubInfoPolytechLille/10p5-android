23083dd245d4ffc2ad0fbdd91f0e62e240229494
JLo'w <Jean-Loup.Beaussart@polytech-lille.net>
2016-05-02 11:47:51 +0200

    Filtre regex pour limiter le nombre de décimale

--- PremiereActivite/app/src/main/java/com/example/app_10p5/TabFragment2.java
+++ PremiereActivite/app/src/main/java/com/example/app_10p5/TabFragment2.java
@@ -2,9 +2,11 @@ package com.example.app_10p5;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.text.InputFilter;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 /**
  * Created by beaus on 24/04/2016.
@@ -14,6 +16,11 @@ public class TabFragment2 extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.tab_fragment_2, container, false);
+        View v = inflater.inflate(R.layout.tab_fragment_2, container, false);;
+
+        EditText et = (EditText) v.findViewById(R.id.commande_prix);
+        et.setFilters(new InputFilter[]{new DecimalDigitsInputFilter(3, 2)});
+
+        return v;
     }
 }
